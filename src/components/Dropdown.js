a23059ee53b2c219a793296dbf227cb490844709
Nikita <saininikita23@gmail.com>
2022-01-11 13:49:45 +0530

    event listener

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const Dropdown  = () => {
-    return <h1>Dropdown</h1>
+const Dropdown  = ({ options, selected,  onSelectedChange }) => {
+
+    const [open, setOpen] = useState(false);
+
+    useEffect(() => {
+        document.body.addEventListener('click', () =>{
+            console.log('BODY CLICK!');
+            setOpen(false);
+        });
+    }, []);
+    const renderedOptions = options.map ((option) => {
+    if(option.value === selected.value){
+        return null;
+    }
+    return (
+        <div 
+        key ={option.value} 
+        className="item"
+        onClick ={ () => {
+            console.log('ITEM CLICKED');
+            onSelectedChange(option)
+            }}
+        >
+            {option.label}
+        </div>
+    );
+});
+
+    return (
+        <div className="ui form">
+            <div className= "field">
+                <label className="label">Select a Color</label>
+                <div 
+                onClick={() => {
+                    console.log('DROPDOWN CLICKED')
+                    setOpen(!open)
+
+                }} 
+                className={`ui selection dropdown ${open ? 'visible active' : '' }`}
+                >
+                    <i className="dropdown icon"></i>
+                    <div className="text">{selected.label}</div>
+                    <div className= {`menu ${open ? 'visible transition' : '' }`}>
+                        {renderedOptions}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Dropdown;
