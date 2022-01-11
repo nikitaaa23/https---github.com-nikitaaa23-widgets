855c4198dc8c16d7196cbbee1abf2cf7810a1456
Nikita <saininikita23@gmail.com>
2022-01-11 15:55:00 +0530

    commit

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -18,10 +18,19 @@ const options = [
 
 const Translate = () => {
 const [language, setLanguage] = useState(options[0])
+const [text, setText] = useState('');
 
     return (
         <div>
-            <Dropdown selected = {language} 
+            <div className="ui form">
+                <div className="field">
+                    <label>Enter Text</label>
+                <input value = {text} onChange ={(e) => setText(e.target.value)} />
+                </div>
+            </div>
+            <Dropdown 
+            label = "Select a Language"
+            selected = {language} 
             onSelectedChnage= {setLanguage} 
             options = {options} 
             />
@@ -30,3 +39,5 @@ const [language, setLanguage] = useState(options[0])
 };
 
 export default Translate;
+
+//AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM
