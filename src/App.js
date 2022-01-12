3d0107d57c5a305407c3c69b97cfc07a071d49e6
Nikita <saininikita23@gmail.com>
2022-01-12 12:12:25 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
 import Route from "./components/Route";
+import Header from "./components/Header";
 
 const items = [
     {
@@ -35,34 +36,6 @@ const options  = [
     }
 ];
 
-const showAccordion = () => {
-    if(window.location.pathname === '/'){
-        return <Accordion items = {items}/>;
-    }
-};
-
-const showList = () =>{
-    if(window.location.pathname === '/list'){
-        return <Search />;
-    }
-};
-
-const showDropdown = () =>{
-    if(window.location.pathname === '/dropdown'){
-        return <Dropdown />;
-    }
-};
-
-const showTranslate = () =>{
-    if(window.location.pathname === '/translate'){
-        return <Translate />;
-    }
-};
-
-const showComponent = (route, component) => {
-    return window.location.pathname === route 
-    ? component: null;
-};
 
 
 
@@ -70,6 +43,7 @@ export default () => {
         const [selected, setSelected] = useState(options[0]);
     return(
     <div>
+       < Header />
         <Route path="/">
             <Accordion items = {items} />
         </Route>
